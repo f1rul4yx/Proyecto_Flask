590a19b3651e202a1985645bc5c36c6d1d11dc9e
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-05-10 23:44:32 +0200

    Mejora 1: Unir la busqueda y los resultados en una única página

--- main/app.py
+++ main/app.py
@@ -19,26 +19,25 @@ with open('static/libros.json') as archivo:
 def index():
     return render_template('index.html')
 
-@app.route('/libros', methods=['GET'])
+@app.route('/libros', methods=['GET', 'POST'])
 def libros():
-    return render_template('libros.html')
-
-@app.route('/listalibros', methods=['POST'])
-def listalibros():
-    consulta = request.form.get('titulo', '')
     libros = []
-    for libro in all_libros:
-        if not consulta or libro['titulo'].lower().startswith(consulta.lower()):
-            libros.append(libro)
-    return render_template('listalibros.html', libros=libros)
+    consulta = ''
+    busqueda = False
+    if request.method == 'POST':
+        busqueda = True
+        consulta = request.form.get('titulo', '')
+        for libro in all_libros:
+            if not consulta or libro['titulo'].lower().startswith(consulta.lower()):
+                libros.append(libro)
+    return render_template('libros.html', libros=libros, consulta=consulta, busqueda=busqueda)
 
 @app.route('/libro/<int:id>')
 def libro(id):
     libro_encontrado = None
     for libro in all_libros:
         if libro['id'] == id:
-            libro_encontrado = libro
-            
+            libro_encontrado = libro            
     if libro_encontrado is None:
         abort(404)
     return render_template('libro.html', libro=libro_encontrado)
